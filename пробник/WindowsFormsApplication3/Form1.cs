b5ff71a82f1cd4c683bb342e304ccc5586442e8a
EgorikMesh <meshingeorge@gmail.com>
2017-05-04 16:10:34 +0300

    Add files via upload

---
+++ пробник/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // double a;
+
+            String[] strLabel = {"Ананас","Арбуз","Киви","Апельсин","Манго","Яблоко","Виноград","Мандарин"};
+            Random r = new Random();
+            int iSelect = r.Next(0, 7);
+            textBox1.Text = strLabel[iSelect];
+
+            textBox3.Text = "я сожрал много\r\n" + textBox1.Text + "\r\n" + r + "\r\n" + textBox2 + "\r\nи все\r\n" + textBox3;
+
+           // a = Convert.ToDouble(textBox4.Text);
+
+
+            //for (int i = 0; i == a; i++) {}
+            saveFileDialog1.FileName = "Новый HTML файл";
+            saveFileDialog1.Filter = "HTML File | *.txt";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            string filename = saveFileDialog1.FileName;
+            System.IO.File.WriteAllText(filename, textBox3.Text);
+            MessageBox.Show("Файл сохранен");
+        }
+    }
+}
